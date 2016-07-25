17f5c3791381f75666bdc7fc7c9e194750f973a4
Hernan Y.Ke <yuriqiao@gmail.com>
2016-07-25 09:06:15 -0400

    basic vowel test

---
+++ vowel.py
@@ -0,0 +1,13 @@
+__author__ = 'Hernan Y.Ke'
+import scrabble
+
+vowels ="aeiou"
+def has_all_vowel(word):
+    for vowel in vowels:
+        if vowel not in word:
+            return False
+    return True
+
+for word in scrabble.wordlist:
+    if has_all_vowel(word):
+        print(word)
