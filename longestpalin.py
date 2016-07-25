4f071d2443d7e6e8f17b07c1010f62d2c2b48d37
Hernan Y.Ke <yuriqiao@gmail.com>
2016-07-25 09:23:03 -0400

    reversed

--- longestpalin.py
+++ longestpalin.py
@@ -2,10 +2,6 @@ __author__ = 'Hernan Y.Ke'
 import scrabble
 longest=""
 for word in scrabble.wordlist:
-    ispalin=True
-    for index in range(len(word)):
-        if word[index]!=word[-(index+1)]:
-            ispalin=False
-    if ispalin and len(word)>len(longest):
+    if list(word) == list(reversed(word)) and len(word) > len(longest):
         longest=word
 print(longest)
